abd5f7a51c3dd5651c04a5d81554091f45180119
Damilola Alao <oluwadamilola.alao@gmail.com>
2021-02-03 11:53:18 +0100

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5dbd2b41fbff6a22dd0dbafa9ee10736'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c80f95dc-efa3-4378-a5d0-98ccf6b10edf'
+};
